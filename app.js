5e653ac5813d9ba484ca3fb8fe1eee21d83c3007
Matt Richards <matt@lucidmoon.co.uk>
2013-01-23 20:15:35 +0000

    Global refactor Abstract core functionality to submodule Test Suite NODE_ENV support Documentation

--- app.js
+++ app.js
@@ -1,42 +1,9 @@
-var _ = require('lodash'),
-	config = require('./app/config/config'),
-	restify = require('restify'),  
-	mongoose = require('mongoose'),
-	db = mongoose.connect(config.credentials.mongoose_auth),
-	notesController = require("./app/controllers/notes");
+//     NoteToSelf API 
+//     (c) 2012-2013 Matt Richards, Lucidmoon Ltd
+//     http://lucidmoon.co.uk
+//     -----
 
-
-var createServer = function (options) {
-    var app = restify.createServer(options);
-    var allowCrossDomain = function (req, res, next) {
-        res.header('Access-Control-Allow-Origin', '*');
-        res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
-        res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
-        
-        // intercept OPTIONS method
-        if ('OPTIONS' == req.method) {
-          res.send(200);
-        }
-        else {
-          next();
-        }
-    };
-
-    app.use(restify.bodyParser());
-    app.pre(allowCrossDomain);    
-    app.get('/notes/:id', notesController.get);
-    app.get('/notes', notesController.get);
-    app.post('/notes', notesController.post);
-    app.put('/notes/:id', notesController.put);    
-    app.del('/notes/:id', notesController.del);
-
-    return app;
-}
-
-var server = createServer({
-	'name': config.app.name
-});
-
-server.listen(config.app.port, function(req, res) {
-	console.log('%s listening at %s', server.name, server.url);
-});
+//  Server module dependency.
+var server = require("./server.js");
+//  Start the server.
+server.start();
--- app/config/config.js
+++ app/config/config.js
@@ -1,8 +1,25 @@
-exports.credentials = {
-  mongoose_auth: 'mongodb://127.0.0.1/notetoself',
-};
+//     NoteToSelf API 
+//     (c) 2012-2013 Matt Richards, Lucidmoon Ltd
+//     http://lucidmoon.co.uk
+//     -----
 
-exports.app = {
-	name: "NoteToSelf API",
-	port: "3030"
-}
+//  Application config.
+var Config = function () {
+    return {
+        name: "NoteToSelf API",
+        port: "3030",
+        mongoose: {
+            development: {
+                connectionString: 'mongodb://127.0.0.1/notetoself'  
+            },
+            testing: {
+                connectionString: 'mongodb://127.0.0.1/notetoself-testing'  
+            },
+            production: {
+                connectionString: 'mongodb://127.0.0.1/notetoself-production'   
+            }               
+        }
+    }
+}();
+
+exports.module = exports = Config;
